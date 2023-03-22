7e352bc1c3d428768d1ad620c5975bef82737b3e
hojat ghasemi <hjt.ghasmi@gmail.com>
2023-03-22 08:28:24 +0430

    implement the edit content component actions

--- src/components/editContent.jsx
+++ src/components/editContent.jsx
@@ -1,18 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { Divider } from "@mui/material";
+import contentService from "../services/contentService";
+import { toast } from "react-toastify";
+
+let initialState = { dua: "", wellcome: "" };
 
 const EditContent = () => {
-  const [duaContent, setDuaContent] = useState(
-    "Allahumma la sahla illa ma ja'altahu sahla, wa Anta taj'alul hazna idha shi'ta sahla'' Translations: '' O Allah, there is noting easy expect ehat You make easy, and You can make hardship easy if You will."
-  );
-  const [wlcomeContent, setWlcomeContent] = useState(
-    "The best day on which the sun has risen is Friday. On it, Adam was created, on it, he was admitted to Paradise, and on it, he was expelled from it. And the last hour will take place on no day other than Friday. (Sahih Muslim 854)."
-  );
+  const [duaContent, setDuaContent] = useState("");
+  const [wlcomeContent, setWlcomeContent] = useState("");
+  const [bool, forceRender] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await contentService.getContent();
+      initialState = data;
+      setDuaContent(data.dua);
+      setWlcomeContent(data.wellcome);
+    };
+
+    fetchData();
+  }, []);
+
+  const isChanged = () => {
+    return (
+      initialState.dua !== duaContent || initialState.wellcome !== wlcomeContent
+    );
+  };
+
+  const handleDiscardChanges = () => {
+    setDuaContent(initialState.dua);
+    setWlcomeContent(initialState.wellcome);
+  };
+
+  const handleSaveChanges = async () => {
+    try {
+      await contentService.saveContent(duaContent, wlcomeContent);
+      toast.success("Content updated successfully.");
+      initialState = { dua: duaContent, wellcome: wlcomeContent };
+      forceRender(!bool);
+    } catch (ex) {
+      toast.error(ex.response.data);
+    }
+  };
 
   return (
     <Container sx={{ width: 1 }}>
@@ -23,44 +56,42 @@ const EditContent = () => {
             mx: "auto",
           }}
         >
-          <Typography component="h3" variant="h4" sx={{ mb: 2 }}>
-            Dua Content
+          <Typography component="h3" variant="h4" sx={{ mb: 4 }}>
+            Content Management
           </Typography>
-          <TextField
-            value={duaContent}
-            multiline
-            fullWidth
-            onChange={(e) => setDuaContent(e.target.value)}
-          />
-          <Box sx={{ mt: 3 }}>
-            <Button variant="contained">Save</Button>
-            <Button variant="outlined" color="error" sx={{ ml: 1 }}>
-              Discard changes
-            </Button>
+          <Box sx={{ mb: 3 }}>
+            <TextField
+              value={duaContent}
+              multiline
+              fullWidth
+              label="Dua Text"
+              onChange={(e) => setDuaContent(e.target.value)}
+            />
           </Box>
-        </Box>
-      </Box>
+          <Box sx={{ mb: 3 }}>
+            <TextField
+              value={wlcomeContent}
+              multiline
+              fullWidth
+              label="Wellcome Text"
+              onChange={(e) => setWlcomeContent(e.target.value)}
+            />
+          </Box>
 
-      <Divider sx={{ my: 7 }} />
-      <Box sx={{ width: 1 }}>
-        <Box
-          sx={{
-            maxWidth: 600,
-            mx: "auto",
-          }}
-        >
-          <Typography component="h3" variant="h4" sx={{ mb: 2 }}>
-            Wellcome Content
-          </Typography>
-          <TextField
-            value={wlcomeContent}
-            multiline
-            fullWidth
-            onChange={(e) => setWlcomeContent(e.target.value)}
-          />
-          <Box sx={{ mt: 3 }}>
-            <Button variant="contained">Save</Button>
-            <Button variant="outlined" color="error" sx={{ ml: 1 }}>
+          <Box>
+            <Button
+              variant="contained"
+              disabled={!isChanged()}
+              onClick={handleSaveChanges}
+            >
+              Save
+            </Button>
+            <Button
+              variant="outlined"
+              color="error"
+              sx={{ ml: 1 }}
+              onClick={handleDiscardChanges}
+            >
               Discard changes
             </Button>
           </Box>
